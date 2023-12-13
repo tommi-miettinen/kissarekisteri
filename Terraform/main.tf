ebb77d72efed4d330c2ebe01cdc98978e44d45a4
unknown <tomppa.miettinen1@gmail.com>
2023-12-13 17:46:20 +0200

    add adb2c directory to terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.84.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.46.0"
+    }
   }
 }
 
@@ -11,35 +15,37 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  rg_name  = "kissarekisteri"
-  location = "northeurope"
-}
-
-
+provider "azuread" {}
 
 /*
-variable sql_admin_password {
+variable "sql_admin_password" {
   description = "The admin password for SQL Server"
   type        = string
   sensitive   = true
 }
 */
 
-
-
-
-
-resource "azurerm_resource_group" "rg" {
-  name     = local.rg_name
-  location = local.location
+resource "azurerm_aadb2c_directory" "kissarekisteriAdB2C" {
+  data_residency_location = "Europe"
+  domain_name             = "kissarekisteri.onmicrosoft.com"
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku_name                = "PremiumP1"
 }
 
+resource "azuread_application" "kissarekisteriAuth" {
+  display_name = "test"
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "kissarekisteri"
+  location = "northeurope"
+}
 
 resource "azurerm_storage_account" "sa" {
   name                     = "kissarekisteritf"
-  resource_group_name      = local.rg_name
-  location                 = local.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
@@ -53,48 +59,39 @@ resource "azurerm_storage_account" "sa" {
       max_age_in_seconds = 200
     }
   }
-
-  depends_on = [azurerm_resource_group.rg]
 }
 
-
-/*
-resource azurerm_sql_server sqlserver {
+resource "azurerm_mssql_server" "sqlserver" {
   name                         = "kissarekisterisqlserver"
-  resource_group_name          = locals.rg_name
-  location                     = locals.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "kissarekisterisqladmin"
-  administrator_login_password =  var.sql_admin_password
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
-resource azurerm_sql_database sql {
-  name                = locals.sql_database_name
-  resource_group_name = locals.rg_name
-  location            = locals.rg.location
-  server_name         = azurerm_sql_server.sqlserver.name
 
+resource "azurerm_mssql_database" "sql" {
+  name      = "kissarekisteridb"
+  server_id = azurerm_mssql_server.sqlserver.id
+  sku_name  = "Basic"
 }
-*/
 
 
-resource "azurerm_service_plan" "example" {
+resource "azurerm_service_plan" "kissarekisterisp" {
   name                = "kissarekisterisp"
-  location            = local.location
-  resource_group_name = local.rg_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Windows"
   sku_name            = "F1"
-
-  depends_on = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_windows_web_app" "appservice" {
   name                = "kissarekisteri-app"
-  location            = local.location
-  resource_group_name = local.rg_name
-  service_plan_id     = azurerm_service_plan.example.id
-
-
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.kissarekisterisp.id
+  https_only          = true
 
   site_config {
     always_on = false
@@ -102,16 +99,30 @@ resource "azurerm_windows_web_app" "appservice" {
       current_stack  = "dotnet"
       dotnet_version = "v8.0"
     }
-
   }
-  https_only = true
 
-  depends_on = [azurerm_resource_group.rg, azurerm_service_plan.example]
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-
-output "app_service_publish_profile" {
-  description = "Azure App Services publish profile, add this to github secrets for deployment workflow"
-  value       = azurerm_windows_web_app.appservice.site_credential
-  sensitive   = true
+resource "azurerm_role_assignment" "sqlrole" {
+  scope                = azurerm_mssql_server.sqlserver.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_windows_web_app.appservice.identity[0].principal_id
 }
+
+/*
+resource "null_resource" "sql_script" {
+  provisioner "local-exec" {
+    command = <<EOT
+      az sql db exec --resource-group ${local.rg_name} \
+                     --server ${azurerm_sql_server.sqlserver.name} \
+                     --name ${azurerm_sql_database.sql.name} \
+                     --sql "CREATE USER ${azurerm_windows_web_app.appservice.name} FROM EXTERNAL PROVIDER;"
+    EOT
+  }
+
+  depends_on = [azurerm_sql_database.sql]
+}
+*/
