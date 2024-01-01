dbadb48602a7d8b49ecf6cb8af882339adf1c50c
unknown <tomppa.miettinen1@gmail.com>
2024-01-01 17:46:07 +0200

    add plugin for using top level await

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 import vue from "@vitejs/plugin-vue";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), basicSsl()],
+  plugins: [
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+    vue(),
+    basicSsl(),
+  ],
 });
